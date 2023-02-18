5d7a17356ae5e8e55703e8a7a02b88cd06e0cdb7
Vishal Manjhi <vmanjhi98@gmail.com>
2023-02-19 02:28:06 +0530

    frist commit

--- src/components/FormUsingReducer.js
+++ src/components/FormUsingReducer.js
@@ -18,9 +18,46 @@ const initialFormState = {
 const FormUsingReducer = () => {
     const [formState,dispatch] = useReducer(signUpFormReducer,initialFormState) 
 
-
+    const onInput = (e) =>{
+        //console.log({type:'input',payload:{field:e.target.id,value:e.target.value}});
+        dispatch({type:'input',payload:{field:e.target.id,value:e.target.value}})
+    }
+    const onFormSubmit = (e) =>{
+        console.log('entette');
+        e.preventDefault()
+        dispatch({type:'resetErrors'})
+        const errors = signUpFormValidation(formState.input)
+        if(errors !== null){
+            console.log(errors);
+            for (const m in errors){
+                dispatch({type:'error',payload:{field:m,value:errors[m]}})
+            }
+        } else {
+            alert('form submitted successfully!!')
+        }
+    }
+    
     return (
-
+        <form id="reducer-form" onSubmit={onFormSubmit}>
+            <label htmlFor="name">Name </label>
+            <input type="text" id="name" value={formState["name"]} onChange={onInput} />
+            {formState.errors["name"]}
+            <br />
+            <br />
+            <label htmlFor="password">Password </label>
+            <input type="text" id="password"  value={formState["password"]} onChange={onInput}/>
+            {formState.errors["password"]}
+            <br />
+            <br />
+            <label htmlFor="email">Email </label>
+            <input type="text" id="email" value={formState["email"]} onChange={onInput} />
+            {formState.errors["email"]}
+            <br />
+            <br />
+            <label htmlFor="consent">Send marketing emails? </label>
+            <input type="checkbox" id="consent" checked={formState.consent} />
+            <button type="submit">Sign Up</button>
+        </form>
     )
 }
 
